9d48ec72bccf8952242f40b95dc543c5ef2b81fb
dhinesh-000 <104247616+dhinesh-000@users.noreply.github.com>
2022-08-03 00:26:39 +0530

    ,

--- proto2/scripts/sphereprefabpropertiesSCROBJ.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName ="SCROBJ/Prefab/SPHERE")]
-public class sphereprefabpropertiesSCROBJ : ScriptableObject
-{
-    public float traveldistance;
-    public float timetocomplete;
-    public float harvestamount;
-}
